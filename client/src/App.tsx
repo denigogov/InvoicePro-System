693a60ccb211468180b97d4f8db66a57cbfb3879
denigogov <122295314+denigogov@users.noreply.github.com>
2024-03-22 13:16:48 +0100

    project setup inital setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+interface AppProps {}
+
+const App: React.FC<AppProps> = ({}) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(compression());
+app.use(helmet());
+
+app.disable("x-powered-by");
+
+const port = process.env.SERVER_PORT || 4001;
+
+const welcomeMessage = (_, res) => {
+  res.send("Backend App Server v0.0.1");
+};
+
+app.use("/", welcomeMessage);
+
+app.listen(port, (err) => {
+  err
+    ? "Something bad happen"
+    : `Server is listening on http://localhost:${port}/`;
+});
+module.exports = { app };
